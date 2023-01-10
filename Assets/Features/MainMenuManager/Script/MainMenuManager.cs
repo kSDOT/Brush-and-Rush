33a0e7f580ae30e367db30502ca2dd6940a642a4
Leo Keil <leolele19@gmail.com>
2023-01-10 18:55:21 +0100

    Added Text Mesh Pro and Changed Folder Structure

--- Assets/Features/MainMenuManager/Script/MainMenuManager.cs
+++ Assets/Features/MainMenuManager/Script/MainMenuManager.cs
@@ -6,6 +6,7 @@ using NaughtyAttributes;
 public class MainMenuManager : MonoBehaviour
 {
     //-----Serialized Variables-----
+    [InfoBox("You can add missing Scenes from the Build Settings menu.", EInfoBoxType.Normal)]
     [SerializeField]
     [Scene]
     private string mainLevelScene;
